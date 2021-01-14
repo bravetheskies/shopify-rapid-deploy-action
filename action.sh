eed3916fa1aafc3827fe74e575dcfe7e5745ac5d
Andrew Wise <andrew@andrewwise.co.uk>
2021-01-14 12:23:31 +0000

    Bad subtitutions fixed and config spelling fixed

--- action.sh
+++ action.sh
@@ -5,7 +5,7 @@ set -e
 curl -s https://shopify.github.io/themekit/scripts/install.py
 
 # Configure Theme Kit
-theme configure --password=${{ secrets.SHOPIFY_APP_API_PASSWORD }} --store=${{ secrets.SHOPIFY_STORE_URL }} --themeid=${{ secrets.SHOPIFY_THEME_ID }} --dir=${{ secrets.THEME_PATH }} --ignored-file=config/settings_data.json --ignored-file=locales/*
+theme configure --password=$SHOPIFY_APP_API_PASSWORD --store=$SHOPIFY_STORE_URL --themeid=$SHOPIFY_THEME_ID --dir=$THEME_PATH --ignored-file=config/settings_data.json --ignored-file=locales/*
 
 # Deploying to theme (allowing live themes)
 theme deploy --allow-live
