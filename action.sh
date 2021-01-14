209eb2944535faefde86ee0160afc5b78cda4849
Andrew Wise <andrew@andrewwise.co.uk>
2021-01-14 12:27:48 +0000

    Themekit install fixed

--- action.sh
+++ action.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Install Theme Kit
-curl -s https://shopify.github.io/themekit/scripts/install.py
+curl -s https://shopify.github.io/themekit/scripts/install.py | sudo python
 
 # Configure Theme Kit
 theme configure --password=$SHOPIFY_APP_API_PASSWORD --store=$SHOPIFY_STORE_URL --themeid=$SHOPIFY_THEME_ID --dir=$THEME_PATH --ignored-file=config/settings_data.json --ignored-file=locales/*
