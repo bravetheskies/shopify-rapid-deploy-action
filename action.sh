5f7e321c03f306b5e2a41146bbea755cba0543a3
Andrew Wise <andrew@andrewwise.co.uk>
2021-01-14 12:50:54 +0000

    Comments changed to echo

--- action.sh
+++ action.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -e
 
-# Install Theme Kit
+echo "Installing Theme Kit"
 curl -s https://shopify.github.io/themekit/scripts/install.py | sudo python
 
-# Configure Theme Kit
+echo "Configuring Theme Kit"
 theme configure --password=$SHOPIFY_APP_API_PASSWORD --store=$SHOPIFY_STORE_URL --themeid=$SHOPIFY_THEME_ID --dir=$THEME_PATH --ignored-file=config/settings_data.json --ignored-file=locales/*
 
-# Deploying to theme (allowing live themes)
-sudo theme deploy --allow-live
+echo "Deploying Theme"
+theme deploy --allow-live
