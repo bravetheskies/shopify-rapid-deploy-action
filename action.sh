6fbc3bc962545d96386eead99b5d782b6b753e89
Andrew Wise <andrew@andrewwise.co.uk>
2021-02-04 09:06:00 +0000

    Location of Theme Kit installed changed, updated in action

--- action.sh
+++ action.sh
@@ -2,7 +2,7 @@
 set -e
 
 echo "Installing Theme Kit"
-curl -s https://shopify.github.io/themekit/scripts/install.py | sudo python
+curl -s https://raw.githubusercontent.com/Shopify/themekit/master/scripts/install.py | sudo python
 
 echo "Configuring Theme Kit"
 theme configure --password=$SHOPIFY_APP_API_PASSWORD --store=$SHOPIFY_STORE_URL --themeid=$SHOPIFY_THEME_ID --dir=$THEME_PATH $THEMEKIT_FLAGS
